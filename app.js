ee8520da0f692864c11c7300cf23956772532d43
perf <electronicfan@proton.me>
2025-02-23 16:08:29 +0100

    Create getComputerChoice function

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('hello');
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * 3 + 1);
+  switch (random) {
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors';
+  }
+}
